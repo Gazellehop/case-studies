496489b0a0f3933583adcbe172d7bdb7795a7c8a
Pavel Vecera <107406479+Gazellehop@users.noreply.github.com>
2024-05-16 23:27:42 +0200

    Update GRPN_2024-05-16_ Products_table_update.sql
    The first version of query with comments

--- GRPN_2024-05-16_ Products_table_update.sql
+++ GRPN_2024-05-16_ Products_table_update.sql
@@ -1,1 +1,27 @@
-
+--Broken column is_deleted in the table Products
+--The target is to recreate the column in query to get the right results
+--assumptions:
+-- - product_id is primal key
+-- - in order to have historical changes in this table this column exists to see log of updates
+-- - the product_id is same for all updated rows
+-- - to select only viable product you would have to select only is_deleted = FALSE
+SELECT
+    product_id
+    ,name
+    ,Category
+    ,price_per_unit
+    ,Updated_at
+    ,CASE
+      WHEN rn = 1 then "FALSE"
+      ELSE "TRUE"
+    END AS is_deleted
+  FROM
+    (SELECT 
+        product_id
+        ,name
+        ,Category
+        ,price_per_unit
+        ,Updated_at
+        ,ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY updated_at desc) as rn
+      FROM
+        Products)
