dd489e5254e6faeb751af171ccc4185117aaeda6
Pavel Vecera <107406479+Gazellehop@users.noreply.github.com>
2024-05-17 11:45:29 +0200

    Update GRPN_2024-05_cohortAnalysis.sql
    A draft of the script without comments

--- GRPN_2024-05_cohortAnalysis.sql
+++ GRPN_2024-05_cohortAnalysis.sql
@@ -12,5 +12,38 @@ payment AS (
     SELECT
         sale_id
         ,to_date(timestamp) AS order_date
+      FROM
+        Payment 
     GROUP 1,2 --Removing of duplicate sale_ids because one sale_id can contain multiple product_ids => in this case we only need sale_id and timestamp
 ),
+customers_orders AS (
+    SELECT
+        customer_id
+        ,CASE
+          WHEN date_from >= order_date and date_to <= order_date then 1
+          ELSE 0
+        END AS firstmonth
+      FROM  
+        (SELECT
+            cust.customer_id
+            ,cust.registration_timestamp
+            ,cust.date_from
+            ,cust.date_to
+            ,pay.sale_id
+            ,pay.order_date
+        FROM
+          customers_group AS cust
+        LEFT JOIN
+          payment AS pay
+        ON cust.customer_id = pay_customer_id
+        )
+      WHERE
+        sale_id IS NOT null --We need to remove customer_ids that have no purchase history
+      GROUP BY 
+        customer_id, firstmonth
+),
+    
+SELECT
+    ROUND(100 * COUNT(DISTINCT (CASE WHEN firstmonth = 1 THEN customer_id END))/COUNT(DISTINCT customer_id),2) AS percentage
+  FROM
+    customers_orders
