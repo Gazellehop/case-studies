b0044cc5ea9b0f46ed7c8840fa098fdcf02ce25f
Pavel Vecera <107406479+Gazellehop@users.noreply.github.com>
2024-05-17 10:29:51 +0200

    Update GRPN_2024-05_cohortAnalysis.sql
    A Part of the script.

--- GRPN_2024-05_cohortAnalysis.sql
+++ GRPN_2024-05_cohortAnalysis.sql
@@ -1,1 +1,16 @@
-
+WITH
+customers_group AS (
+    SELECT
+        customer_id
+        ,registration_timestamp
+        ,TO_DATE(registration_timestamp)                   AS date_from
+        ,DATEADD(day, 30, TO_DATE(registration_timestamp)  AS date_to
+      FROM
+        Customers    
+),
+payment AS (
+    SELECT
+        sale_id
+        ,to_date(timestamp) AS order_date
+    GROUP 1,2 --Removing of duplicate sale_ids because one sale_id can contain multiple product_ids => in this case we only need sale_id and timestamp
+),
