1bd829747503ac434d0466c998d3afd96c6b1a1b
Pavel Vecera <107406479+Gazellehop@users.noreply.github.com>
2024-05-18 21:21:43 +0200

    Update GRPN_2024-05_revenueForecasting.sql
    Comments

--- GRPN_2024-05_revenueForecasting.sql
+++ GRPN_2024-05_revenueForecasting.sql
@@ -1,7 +1,7 @@
 --Revenue Forecasting: Provide a query that predicts the total revenue for the next quarter based on the average growth rate of the past three complete quarters
 --Warning: Extremely imprecise method for calculating prognosis! In this case there are no ways how to avoid seasonality bias. Extrapolation are better to use, but much more complex.
---Sorry, honestly I am not an expert on time series, so I choose princip below. I found my inspiration in https://docs.snowflake.com/en/user-guide/functions-window-using: Calculating a 3-Day Moving Average.
---Snowflake script (beware! this script has not been validated yet, it is only a draft and you may find errors there):
+--Sorry, honestly I am not an expert on time series, so I selected princip below. I found my inspiration in https://docs.snowflake.com/en/user-guide/functions-window-using: Calculating a 3-Day Moving Average.
+--Snowflake script:
 WITH
 revenue_quarters AS ( --I want to get source with revenue by individual quarters. 
     SELECT 
@@ -13,9 +13,9 @@ revenue_quarters AS ( --I want to get source with revenue by individual quarters
 )
 
 SELECT
-    LEFT(quarter,7)  AS quarter --I changed the format to quarter YYYY-QQ
+    LEFT(quarter,7)  AS quarter --I changed the format to YYYY-QQ
     ,revenue_actuals
-    AVG(revenue_actuals) OVER (ORDER BY quarter ROWS BETWEEN 3 PRECEDING AND 1 PRECEDING) AS forecasting --3 PRECEDING AND 1 PRECEDING because I want to get forecast from the complete 3 quarters
+    AVG(revenue_actuals) OVER (ORDER BY quarter ROWS BETWEEN 3 PRECEDING AND 1 PRECEDING) AS forecasting --3 PRECEDING AND 1 PRECEDING because I want to get forecast from the 3 previous complete quarters
   FROM 
     revenue_quarters
   ORDER BY
